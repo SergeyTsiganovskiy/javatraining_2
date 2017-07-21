930161daeb57b61ab8bd2e57b85bc0eac3b5e86d
tsyganovskiy <tsyganovskiySS@gmail.com>
2017-07-21 20:13:09 +0300

    Оптимизация переходов между страницами

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -3,7 +3,7 @@ package ru.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class NavigationHelper extends HelperBase{
+public class NavigationHelper extends HelperBase {
 
 
   public NavigationHelper(WebDriver wd) {
@@ -11,7 +11,13 @@ public class NavigationHelper extends HelperBase{
   }
 
   public void gotoGroupPage() {
-      click(By.linkText("groups"));
+
+    if (isElementPresent(By.tagName("h1"))
+            && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+            && isElementPresent(By.name("new"))) {
+      return;
+    }
+    click(By.linkText("groups"));
   }
 
   public void gotoContactPage() {
@@ -19,6 +25,10 @@ public class NavigationHelper extends HelperBase{
   }
 
   public void gotoHomePage() {
+
+    if (isElementPresent(By.id("maintable"))) {
+      return;
+    }
     click(By.linkText("home"));
   }
 }
